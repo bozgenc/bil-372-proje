dfaf3cf69abecacafefb8d23a323049d114d76d3
bozgenc <baranzgn@icloud.com>
2020-12-06 00:01:28 +0300

    register page created

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__, template_folder="/Users/baranozgenc/Desktop/proje/Bil372Proje/templates")
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345@localhost:5432/projeDB"
+db = SQLAlchemy(app)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        tckn = request.form['tckn']
+        password = request.form['password']
+        password2 = request.form['password2']
+
+        roleQuery = "SELECT * FROM personel WHERE tckn =  '" + tckn + "' "
+        temp = db.session.execute(roleQuery)
+        userType = ""
+        for x in temp:
+            userType = x.userrole
+
+        if password == password2:
+            query = "INSERT INTO Login(tckn, passcode, userRole) VALUES ('" + tckn + "' , '" + password + "', '" + userType + "')"
+            result = db.engine.execute(query)
+
+    return render_template("register.html",)
 
 
 @app.route("/")
