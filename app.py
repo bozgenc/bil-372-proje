30be3ba6e1a379bfdd8d413b1dbf65493fa6d0d3
bozgenc <baranzgn@icloud.com>
2020-12-06 22:14:54 +0300

    home page for nakliyeci and stylings for login/register

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -33,6 +33,7 @@ def register():
 
     return render_template("register.html",)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -43,7 +44,6 @@ def login():
         query = "SELECT * FROM public.\"Login\" WHERE tckn =  '" + tckn + "' "
         temp = db.session.execute(query)
         rows = temp.fetchall()
-
 
         if len(rows) == 0:
             flash("No user found in the database with the TCKN " + tckn, 'error')
@@ -56,14 +56,45 @@ def login():
                 if userrole == "koordinator":
                     return render_template("index.html")
                 elif userrole == "nakliyeci":
-                    return render_template("nakliyeciHome.html")
+                    return redirect(url_for("nakliyeciHome"), code=303)
             else:
                 flash("Password is incorrect, please try again!")
                 return render_template("login.html")
                 #else:
                     #return render_template("admin.html")
+
     else:
         return render_template("login.html")
+
+
+@app.route("/nakliyeciHome", methods=['GET','POST'])
+def nakliyeciHome():
+    if request.method == 'POST':
+        uretici = request.form['uretici']
+        aciklama = request.form['aciklama']
+        miktar = request.form['miktar']
+        arac = request.form['arac']
+        odeme = request.form['odeme']
+
+        query = "SELECT * FROM public.\"Uretici\" WHERE ad_soyad =  '" + uretici + "' "
+        res = db.session.execute(query)
+        res = res.fetchall()
+        uretici_tckn = res[0].tckn
+
+        insertQuery = "INSERT INTO public.\"Satin_Alir\"(uretici_tckn, aciklama, odeme_miktari ,urun_miktari, plaka) VALUES ('" + uretici_tckn + "' , '" + aciklama + "' , '" + odeme + "' ,'" + miktar + "', '" + arac + "')"
+        db.engine.execute(insertQuery)
+
+
+    queryUretici = "SELECT * FROM public.\"Uretici\""
+    temp = db.session.execute(queryUretici)
+
+    queryArac = "SELECT * FROM public.\"Arac\""
+    temp2 = db.session.execute(queryArac)
+
+    uList = temp.fetchall()
+    aList = temp2.fetchall()
+
+    return render_template("nakliyeciHome.html", ureticiList=uList, aracList=aList)
 
 
 @app.route("/")
