b3adf494c3663f3456e1e56755bfb444b68113c9
Arda Isker <aisker@etu.edu.tr>
2020-12-08 02:49:20 +0300

    Feedback sekmesi eklendi

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request,flash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from sqlalchemy_utils.functions import database_exists
 
-
-app = Flask(__name__, template_folder="/Users/baranozgenc/Desktop/proje/Bil372Proje/templates")
+app = Flask(__name__, template_folder="C:\\Users\\user\\Documents\\GitHub\\Bil372Proje\\templates")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345@localhost:5432/coffeeDB"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:vampirler@localhost:5432/dbV4"
 db = SQLAlchemy(app)
 
 
@@ -16,23 +16,160 @@ def register():
         password = request.form['password']
         password2 = request.form['password2']
 
-        roleQuery = "SELECT * FROM public.\"Personel\" WHERE tckn =  '" + tckn + "' "
+        roleQuery = "SELECT * FROM personel WHERE tckn =  '" + tckn + "' "
         temp = db.session.execute(roleQuery)
-        rows = temp.fetchall()
+        userType = ""
+        for x in temp:
+            userType = x.userrole
 
-        if len(rows) == 0:
-            flash("No personel found to be register with the TCKN: " + tckn, 'error')
-            return render_template("register.html")
-        else:
-            userType = rows[0].personel_tipi
-            if password == password2:
-                query = "INSERT INTO public.\"Login\"(tckn, passcode, personel_tipi) VALUES ('" + tckn + "' , '" + password + "', '" + userType + "')"
-                result = db.engine.execute(query)
-                if result:
-                    flash("Successfully registered, you can log in.")
-                    return render_template("login.html")
+        if password == password2:
+            query = "INSERT INTO Login(tckn, passcode, userRole) VALUES ('" + tckn + "' , '" + password + "', '" + userType + "')"
+            result = db.engine.execute(query)
 
     return render_template("register.html",)
+
+
+
+@app.route("/iletisim", methods=['GET', 'POST'])
+def iletisim():
+    if request.method == 'POST':
+        email = request.form['email']
+        mesaj = request.form['mesaj']
+        if len(mesaj) >0 or  len(email) >0:
+
+            query = "INSERT INTO \"Iletisim\"(email, mesaj) VALUES ('" + email + "' , '" + mesaj +  "')"
+
+            result = db.engine.execute(query)
+
+        else:
+            flash("Lütfen alanları doldurun !")
+
+            return render_template("iletisim.html")
+
+
+    return render_template("iletisim.html",)
+
+
+
+@app.route('/cekirdek', methods=['GET', 'POST'])
+def cekirdek():
+    if request.method == 'POST':
+        miktar = request.form['miktar']
+        tur = request.form['tur']
+        koken = request.form['koken']
+        id = request.form['isInsert']
+
+        intMiktar = int(miktar)
+        print("id:")
+        print(id)
+
+        if int(id) == 0:
+            if intMiktar > 0:
+                query = "INSERT INTO \"Cekirdek\"(koken, miktar, tur) VALUES ('" + koken + "' , '" + miktar + "', '" + tur + "')"
+                result = db.engine.execute(query)
+
+
+            else:
+
+                flash("Miktar sıfırdan fazla olmalı., lütfen tekrar deneyin!")
+
+                return render_template("cekirdek.html")
+
+
+
+        else:
+
+            query_update = "UPDATE public.\"Cekirdek\" SET koken = '" + koken + "', miktar = '" + miktar + "', tur = '" + tur + "' WHERE id = '" + str(
+
+                id) + "'"
+
+            db.engine.execute(query_update)
+
+
+
+
+    queryCekirdek = "SELECT * FROM public.\"Cekirdek\""
+    temp = db.session.execute(queryCekirdek)
+
+    cList = temp.fetchall()
+
+    return render_template("cekirdek.html", cekirdekList=cList)
+
+
+
+@app.route('/paket', methods=['GET', 'POST'])
+def paket():
+    if request.method == 'POST':
+        gramaj = request.form['gramaj']
+        tur = request.form['tur']
+        skt = request.form['skt']
+        id = request.form['isInsert']
+
+        if int(id) == 0:
+            query = "INSERT INTO \"Paket_Kahve\"(gramaj, skt, tur) VALUES ('" + gramaj + "' , '" + skt + "', '" + tur + "')"
+            result = db.engine.execute(query)
+
+        else:
+            query_update = "UPDATE public.\"Paket_Kahve\" SET tur = '" + tur + "', gramaj = '" + gramaj + "', skt = '"+ skt +"' WHERE id = '" + str(id) + "'"
+            db.engine.execute(query_update)
+
+
+
+
+    queryPaket = "SELECT * FROM public.\"Paket_Kahve\""
+    temp = db.session.execute(queryPaket)
+
+    pList = temp.fetchall()
+
+    return render_template("paket.html", paketList=pList)
+
+
+
+
+
+
+@app.route("/cekirdek_delete/<int:id>", methods=['GET','POST'])
+def cekirdekDelete(id):
+    if request.method == 'GET':
+        query = "DELETE FROM public.\"Cekirdek\" WHERE id = '" + str(id) + "'"
+        db.engine.execute(query)
+
+    queryPaket = "SELECT * FROM public.\"Cekirdek\""
+    temp = db.session.execute(queryPaket)
+
+    cList = temp.fetchall()
+
+    return render_template("paket.html", cekirdekList=cList)
+
+
+
+
+
+
+
+
+
+
+@app.route("/paket_delete/<int:id>", methods=['GET','POST'])
+def paketDelete(id):
+    if request.method == 'GET':
+        query = "DELETE FROM public.\"Paket_Kahve\" WHERE id = '" + str(id) + "'"
+        db.engine.execute(query)
+
+    queryPaket = "SELECT * FROM public.\"Paket_Kahve\""
+    temp = db.session.execute(queryPaket)
+
+    pList = temp.fetchall()
+
+    return render_template("paket.html", paketList=pList)
+
+
+
+
+
+
+
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -42,90 +179,53 @@ def login():
         password = request.form['password']
         passwordFlag = False
 
-        query = "SELECT * FROM public.\"Login\" WHERE tckn =  '" + tckn + "' "
+        query = "SELECT * FROM login WHERE tckn =  '" + tckn + "' "
         temp = db.session.execute(query)
         rows = temp.fetchall()
+
 
         if len(rows) == 0:
             flash("No user found in the database with the TCKN " + tckn, 'error')
             return render_template("login.html")
         else:
-            userrole = rows[0].personel_tipi
+            userrole = rows[0].userrole
             if password == rows[0].passcode:
                 passwordFlag = True
             if passwordFlag:
                 if userrole == "koordinator":
-                    return redirect(url_for("/"), code=303)
+                    return render_template("index.html")
                 elif userrole == "nakliyeci":
-                    return redirect(url_for("nakliyeciHome"), code=303)
+                    return render_template("nakliyeciHome.html")
             else:
                 flash("Password is incorrect, please try again!")
                 return render_template("login.html")
-
+                #else:
+                    #return render_template("admin.html")
     else:
         return render_template("login.html")
 
 
-@app.route("/nakliyeciHome", methods=['GET','POST'])
-def nakliyeciHome():
-    if request.method == 'POST':
-        uretici = request.form['uretici']
-        aciklama = request.form['aciklama']
-        miktar = request.form['miktar']
-        arac = request.form['arac']
-        odeme = request.form['odeme']
-        id = request.form['isInsert']
-
-        query = "SELECT * FROM public.\"Uretici\" WHERE ad_soyad =  '" + uretici + "'"
-        res = db.session.execute(query)
-        res = res.fetchall()
-        uretici_tckn = res[0].tckn
-        today = datetime.today()
-
-        if int(id) == 0:
-            insertQuery = "INSERT INTO public.\"Satin_Alir\"(uretici_tckn, odeme_tarihi,aciklama, odeme_miktari ,urun_miktari, plaka) VALUES ('" + uretici_tckn + "' , '" + str(today) + "' ,'" + aciklama + "' , '" + odeme + "' ,'" + miktar + "', '" + arac + "')"
-            db.engine.execute(insertQuery)
-
-        else:
-            query_update = "UPDATE public.\"Satin_Alir\" SET uretici_tckn = '" + uretici_tckn + "', odeme_miktari = '" + odeme + "', odeme_tarihi = '" + str(today) + "', urun_miktari = '" + str(miktar) + "', plaka = '" + arac + "'  WHERE id = '" + str(id) + "'"
-            db.engine.execute(query_update)
 
 
-    queryUretici = "SELECT * FROM public.\"Uretici\""
-    temp = db.session.execute(queryUretici)
-    queryArac = "SELECT * FROM public.\"Arac\""
-    temp2 = db.session.execute(queryArac)
-    queryAll = "SELECT public.\"Satin_Alir\".id, public.\"Satin_Alir\".odeme_miktari, public.\"Satin_Alir\".odeme_tarihi, public.\"Satin_Alir\".urun_miktari, public.\"Satin_Alir\".aciklama, public.\"Satin_Alir\".plaka, public.\"Uretici\".ad_soyad FROM( public.\"Satin_Alir\" INNER JOIN public.\"Uretici\" ON public.\"Satin_Alir\".uretici_tckn = public.\"Uretici\".tckn)"
-    temp3 = db.session.execute(queryAll)
-
-    uList = temp.fetchall()
-    aList = temp2.fetchall()
-    purchaseList = temp3.fetchall()
-    return render_template("nakliyeciHome.html", ureticiList=uList, aracList=aList, purchaseList=purchaseList)
 
 
-@app.route("/nakliyeciHome_delete/<int:id>", methods=['GET','POST'])
-def nakliyeciHomeDelete(id):
-    if request.method == 'GET':
-        query = "DELETE FROM public.\"Satin_Alir\" WHERE id = '" + str(id) + "'"
-        db.engine.execute(query)
 
-    queryUretici = "SELECT * FROM public.\"Uretici\""
-    temp = db.session.execute(queryUretici)
-    queryArac = "SELECT * FROM public.\"Arac\""
-    temp2 = db.session.execute(queryArac)
-    queryAll = "SELECT public.\"Satin_Alir\".id, public.\"Satin_Alir\".odeme_miktari, public.\"Satin_Alir\".odeme_tarihi, public.\"Satin_Alir\".urun_miktari, public.\"Satin_Alir\".aciklama, public.\"Satin_Alir\".plaka, public.\"Uretici\".ad_soyad FROM( public.\"Satin_Alir\" INNER JOIN public.\"Uretici\" ON public.\"Satin_Alir\".uretici_tckn = public.\"Uretici\".tckn)"
-    temp3 = db.session.execute(queryAll)
 
-    uList = temp.fetchall()
-    aList = temp2.fetchall()
-    purchaseList = temp3.fetchall()
-    return render_template("nakliyeciHome.html", ureticiList=uList, aracList=aList, purchaseList=purchaseList)
+
 
 
 @app.route("/")
 def home():
-    return render_template("homePage.html")
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
