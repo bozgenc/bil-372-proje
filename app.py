8031a11ee7b201efcbe229b82388b7933666daa9
bozgenc <baranzgn@icloud.com>
2020-12-08 19:41:31 +0300

    admin page araclar search added

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ def adminHome():
                 id) + "'"
             db.engine.execute(update_query)
 
-    query = "SELECT * FROM public.\"userTypes\""
+    query = "SELECT * FROM public.\"usertypes\""
     temp = db.session.execute(query)
     queryPersonel = "SELECT * FROM public.\"Personel\""
     temp2 = db.session.execute(queryPersonel)
@@ -216,7 +216,7 @@ def adminHomeDelete(id):
         query2 = "DELETE FROM public.\"Personel\" WHERE tckn = '" + str(id) + "'"
         db.engine.execute(query2)
 
-    query = "SELECT * FROM public.\"userTypes\""
+    query = "SELECT * FROM public.\"usertypes\""
     temp = db.session.execute(query)
     queryPersonel = "SELECT * FROM public.\"Personel\""
     temp2 = db.session.execute(queryPersonel)
@@ -235,13 +235,25 @@ def admin_search():
             query = "SELECT public.\"Personel\".* FROM public.\"Personel\" INNER JOIN ( SELECT public.\"Personel\".tckn AS pg_search_id,(ts_rank((to_tsvector('english', coalesce(public.\"Personel\".ad::text,  '')) || to_tsvector('english', coalesce(public.\"Personel\".soyad::text, '')) || to_tsvector('english', coalesce(public.\"Personel\".email::text, ''))),(to_tsquery('english', ''' ' || '" + search + "'|| ' ''')))) FROM public.\"Personel\" WHERE (((to_tsvector('english', coalesce(public.\"Personel\".ad::text, '')) ||to_tsvector('english', coalesce(public.\"Personel\".soyad::text, '')) ||to_tsvector('english', coalesce(public.\"Personel\".email::text, ''))) @@ (to_tsquery('english', ''' ' || '"+ search + " '|| ' '''))))) pg_search ON public.\"Personel\".tckn= pg_search.pg_search_id LIMIT 24 OFFSET 0"
             t = db.engine.execute(query)
             list = t.fetchall()
-            query = "SELECT * FROM public.\"userTypes\""
+            query = "SELECT * FROM public.\"usertypes\""
             temp = db.session.execute(query)
             queryPersonel = "SELECT * FROM public.\"Personel\""
             temp2 = db.session.execute(queryPersonel)
 
             roleList = temp.fetchall()
             return render_template("adminHome.html", roleList=roleList, personelList=list, son=list)
+
+@app.route("/araclar_search", methods=['GET', 'POST'])
+def araclar_search():
+    if request.method == 'POST':
+        search = request.form['search']
+
+        if search != ' ' or search != '':
+            query = "SELECT public.\"Arac\".* FROM public.\"Arac\" INNER JOIN (SELECT public.\"Arac\".plaka AS pg_search_id,(ts_rank((to_tsvector('english', coalesce(public.\"Arac\".plaka::text,  ''))),(to_tsquery('english', ''' ' || '"+ search + "' || ' ''')))) FROM public.\"Arac\" WHERE (((to_tsvector('english', coalesce(public.\"Arac\".plaka::text, ''))) @@ (to_tsquery('english', ''' ' || '"  + search + "' || ' '''))))) pg_search ON public.\"Arac\".plaka= pg_search.pg_search_id LIMIT 24 OFFSET 0"
+            t = db.engine.execute(query)
+            list = t.fetchall()
+            return render_template("araclar.html", aracList=list)
+
 
 
 
